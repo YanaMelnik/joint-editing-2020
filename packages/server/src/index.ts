b481ba74975d1e2bda9c443ad0af47f5d7de2319
Yana Melnyk <yana_melnyk@epam.com>
2021-02-10 11:05:29 +0200

    BUG - added hack for correct work of server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,11 +4,39 @@ const http = require('http');
 const webSocket = require('ws');
 const { v4: uuidv4 } = require('uuid');
 
-const textState = 'Test message from server';
+let textState = 'Test message from server';
 
 const server = http.createServer();
 
 const wsServer = new webSocket.Server({ server });
+
+// TODO: BUG - needs to be deleted when fix using library
+function applyTextDiff(text: string, diffs): string {
+  const newString = [];
+  let cursor = 0;
+
+  diffs.forEach((elem) => {
+    if (elem.type === 'added') {
+      newString.push(text.slice(cursor, elem.index));
+      newString.push(elem.value);
+      cursor = elem.index;
+    }
+
+    if (elem.type === 'removed') {
+      if (cursor !== elem.index) {
+        newString.push(text.slice(cursor, elem.index));
+      }
+
+      cursor = elem.index + elem.count;
+    }
+  });
+
+  if (cursor < text.length) {
+    newString.push(text.slice(cursor));
+  }
+
+  return newString.join('');
+}
 
 wsServer.on('connection', (ws) => {
   // eslint-disable-next-line no-param-reassign
@@ -29,7 +57,7 @@ wsServer.on('connection', (ws) => {
     });
 
     // TODO: BUG - can't use shared library, server stopped
-    // textState = applyTextDiff(textState, JSON.parse(data));
+    textState = applyTextDiff(textState, JSON.parse(data));
   });
 });
 
